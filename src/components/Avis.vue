2f1597c7aa8fbb452aad59cc115b4c6c79f4c1d5
Bernou11 <thony95540@hotmail.fr>
2025-12-10 22:19:34 +0100

    Avis

---
+++ src/components/Avis.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import avis from '@/assets/avis.svg';
+</script>
+
+<template>
+  <div class="hidden md:block w-full bg-gray-50">
+    <div class="hidden md:block w-full bg-gray-50">
+      <img :src="avis"
+           alt="Témoignages clients"
+           class="w-full h-auto max-h-[500px] object-contain" />
+    </div>
+  </div>
+</template>
